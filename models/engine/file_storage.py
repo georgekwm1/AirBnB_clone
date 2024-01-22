b5ed735b28c1fb40fb865d82ac4eac91c82e8e10
AOUDAI <aoudair16031999@gmail.com>
2024-01-22 05:29:20 +0100

    fix some codes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,44 +27,17 @@ class FileStorage:
         with open(FileStorage.__file_path, "w") as file:
             my_dict = FileStorage.__objects
             my_dict = {key: my_dict[key].to_dict() for key in my_dict.keys()}
-            classes = [Amenity, BaseModel, City, Place, Review, State, User]
-            for cls in classes:
-                for key, value in cls.to_dict().items():
-                    if key == 'state_id':
-                        value = f"{cls.__name__}.{cls.id}"
-                    if key == 'city_id':
-                        value = f"{cls.__name__}.{cls.id}"
-                    if key == 'user_id':
-                        value = f"{cls.__name__}.{cls.id}"
-                    if key == 'amenity_ids':
-                        value = f"{cls.__name__}.{cls.id}"
-                    if key == 'place_id':
-                        value = f"{cls.__name__}.{cls.id}"
-                    if key == 'user_id':
-                        value = f"{cls.__name__}.{cls.id}"
-                    
             json.dump(my_dict, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects (only if the file exists)"""
         try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+            with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
-            for key, value in data.items():
-                obj_key = key.split('.')
-                class_name = obj_key[0]
-                obj_id = obj_key[1]
-                cls = eval(class_name)
-                obj = cls(**value)
-                FileStorage.__objects[key] = obj
+            for obj in data.values():
+                class_name = obj["__class__"]
+                self.new(eval(class_name)(**obj))
+
         except FileNotFoundError:
             pass  # File doesn't exist yet, no need to do anything
-        except json.decoder.JSONDecodeError:
-            pass  # Handle the case where the file is empty or contains invalid JSON
 
-        
-        my_dict = FileStorage.__objects
-        for key in my_dict.keys():
-            my_dict[key] = BaseModel(**my_dict[key])
-
-        FileStorage.__objects = my_dict
